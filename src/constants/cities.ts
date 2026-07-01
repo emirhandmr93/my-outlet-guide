3cef51a9f36297ff112facf6105b54b756b98c81
emirhandmr93 <emirhandmr93@gmail.com>
2026-07-01 22:10:03 +0000

    Expand United Kingdom outlet coverage

--- src/constants/cities.ts
+++ src/constants/cities.ts
@@ -1,13 +1,58 @@
 export const cities = [
   {
-    "cityId": "paris",
-    "cityName": "Paris",
-    "countryId": "france"
+    "cityId": "ashford",
+    "cityName": "Ashford",
+    "countryId": "united-kingdom"
   },
   {
-    "cityId": "milan",
-    "cityName": "Milan",
-    "countryId": "italy"
+    "cityId": "banbridge",
+    "cityName": "Banbridge",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "birmingham",
+    "cityName": "Birmingham",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "cardiff",
+    "cityName": "Cardiff",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "doncaster",
+    "cityName": "Doncaster",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "durham",
+    "cityName": "Durham",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "gloucester",
+    "cityName": "Gloucester",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "gretna",
+    "cityName": "Gretna",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "hatfield",
+    "cityName": "Hatfield",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "leeds",
+    "cityName": "Leeds",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "liverpool",
+    "cityName": "Liverpool",
+    "countryId": "united-kingdom"
   },
   {
     "cityId": "london",
@@ -15,8 +60,48 @@ export const cities = [
     "countryId": "united-kingdom"
   },
   {
+    "cityId": "milan",
+    "cityName": "Milan",
+    "countryId": "italy"
+  },
+  {
+    "cityId": "nottingham",
+    "cityName": "Nottingham",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "paris",
+    "cityName": "Paris",
+    "countryId": "france"
+  },
+  {
+    "cityId": "portsmouth",
+    "cityName": "Portsmouth",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "spalding",
+    "cityName": "Spalding",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "street",
+    "cityName": "Street",
+    "countryId": "united-kingdom"
+  },
+  {
     "cityId": "stuttgart",
     "cityName": "Stuttgart",
     "countryId": "germany"
+  },
+  {
+    "cityId": "swindon",
+    "cityName": "Swindon",
+    "countryId": "united-kingdom"
+  },
+  {
+    "cityId": "york",
+    "cityName": "York",
+    "countryId": "united-kingdom"
   }
 ];
