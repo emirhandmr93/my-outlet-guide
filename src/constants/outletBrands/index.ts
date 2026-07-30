178ca33370690f70464fb71a5f90229bfa30fabd
emirhandmr93 <emirhandmr93@gmail.com>
2026-07-30 22:43:03 +0300

    Add Dubai Outlet Mall brands

--- src/constants/outletBrands/index.ts
+++ src/constants/outletBrands/index.ts
@@ -24,6 +24,7 @@ import { swedenOutletBrands } from "./sweden";
 import { switzerlandOutletBrands } from "./switzerland";
 import { ukOutletBrands } from "./uk";
 import { turkeyOutletBrands } from "./turkey";
+import { unitedArabEmiratesOutletBrands } from "./united-arab-emirates";
 
 export type OutletBrand = {
   outletId: string;
@@ -59,6 +60,7 @@ export {
   switzerlandOutletBrands,
   ukOutletBrands,
   turkeyOutletBrands,
+  unitedArabEmiratesOutletBrands,
 };
 
 export const outletBrands: OutletBrand[] = [
@@ -88,4 +90,5 @@ export const outletBrands: OutletBrand[] = [
   ...latviaOutletBrands,
   ...lithuaniaOutletBrands,
   ...turkeyOutletBrands,
+  ...unitedArabEmiratesOutletBrands,
 ];
