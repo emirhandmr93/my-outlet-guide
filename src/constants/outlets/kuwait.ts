3814272a7320f5b8fcc60b3aed62c312fdb54754
emirhandmr93 <emirhandmr93@gmail.com>
2026-08-10 00:05:44 +0300

    data: add Dubai and Kuwait outlet batch

---
+++ src/constants/outlets/kuwait.ts
@@ -0,0 +1,29 @@
+export const kuwaitOutlets = [
+  {
+    outletId: "al-khiran-hybrid-outlet-mall",
+    outletName: "Al Khiran Hybrid Outlet Mall",
+    name: "Al Khiran Hybrid Outlet Mall",
+    slug: "al-khiran-hybrid-outlet-mall",
+    countryId: "kuwait",
+    cityId: "al-khiran",
+    address: "Al Khiran, Kuwait",
+    latitude: "",
+    longitude: "",
+    openingHours: "",
+    heroImage: "",
+    galleryImages: [],
+    storesCountText: "284 retail stores",
+    rating: 0,
+    reviewCount: 0,
+    services: [],
+    restaurants: [],
+    parking: "",
+    taxFreeAvailable: false,
+    officialWebsite: "https://www.tamdeen.com/",
+    websiteUrl: "https://www.tamdeen.com/",
+    googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=Al%20Khiran%20Hybrid%20Outlet%20Mall%2C%20Kuwait",
+    appleMapsUrl: "https://maps.apple.com/?q=Al%20Khiran%20Hybrid%20Outlet%20Mall%2C%20Kuwait",
+    yandexMapsUrl: "https://yandex.com/maps/?text=Al%20Khiran%20Hybrid%20Outlet%20Mall%2C%20Kuwait",
+    status: "active"
+  }
+];
