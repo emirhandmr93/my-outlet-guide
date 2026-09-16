732c1db8b3f09a71da13f0780a9c85d19c335abd
emirhandmr93 <emirhandmr93@gmail.com>
2026-09-16 16:04:34 +0300

    Export campaign translation backfill function

--- functions/src/entry.ts
+++ functions/src/entry.ts
@@ -1,2 +1,3 @@
 export * from "./index";
 export { syncTravelpayoutsRevenue } from "./travelpayoutsRevenueSync";
+export { backfillOutletCampaignTranslations } from "./outletCampaignTranslationBackfill";
